bbfa3bfc9e9d7ea4b671809da9d0a211e4a85c0f
Gabriel Suarez (Gabops) <cgabrielsu@gmail.com>
2019-11-08 20:55:18 +0000

    scenario just using the default variables of the role

---
+++ molecule/basic_configuration/tests/test_default.py
@@ -0,0 +1,29 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_appoptics_directory(host):
+    d = host.file('/opt/SolarWinds/Snap/')
+
+    assert d.exists
+    assert d.user == 'solarwinds'
+    assert d.group == 'solarwinds'
+
+
+def test_appoptics_config(host):
+    f = host.file('/opt/SolarWinds/Snap/etc/config.yaml')
+
+    assert f.exists
+    assert f.contains('environment: test')
+    assert f.user == 'solarwinds'
+    assert f.group == 'solarwinds'
+
+
+def test_appoptics_service(host):
+    s = host.service('swisnapd')
+
+    assert s.is_running
---
+++ molecule/plugins_configuration/tests/test_default.py
@@ -0,0 +1,47 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_appoptics_directory(host):
+    d = host.file('/opt/SolarWinds/Snap/')
+
+    assert d.exists
+    assert d.user == 'solarwinds'
+    assert d.group == 'solarwinds'
+
+
+def test_appoptics_config(host):
+    f = host.file('/opt/SolarWinds/Snap/etc/config.yaml')
+
+    assert f.exists
+    assert f.contains('environment: test')
+    assert f.user == 'solarwinds'
+    assert f.group == 'solarwinds'
+
+
+def test_appoptics_plugins(host):
+    f = host.file('/opt/SolarWinds/Snap/etc/plugins.d/plugin-test.yaml')
+
+    assert f.exists
+    assert f.contains('foo:passwd')
+    assert f.user == 'solarwinds'
+    assert f.group == 'solarwinds'
+
+
+def test_appoptics_tasks(host):
+    f = host.file('/opt/SolarWinds/Snap/etc/tasks.d/task-test.yaml')
+
+    assert f.exists
+    assert f.contains('/mysql/aborted/clients')
+    assert f.user == 'solarwinds'
+    assert f.group == 'solarwinds'
+
+
+def test_appoptics_service(host):
+    s = host.service('swisnapd')
+
+    assert s.is_running
